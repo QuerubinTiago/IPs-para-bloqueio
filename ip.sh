eaa062d12d182cfd34c83e02200d63e976ba9ba8
mendesjean <47597223+mendesjean@users.noreply.github.com>
2019-09-10 12:28:17 -0300

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # 
+iptables -I INPUT -s 62.210.251.231 -j DROP
+iptables -I INPUT -s 46.166.187.89 -j DROP
 iptables -I INPUT -s 77.247.110.216 -j DROP
 iptables -I INPUT -s 144.76.116.208 -j DROP
 iptables -I INPUT -s 62.210.251.205 -j DROP
